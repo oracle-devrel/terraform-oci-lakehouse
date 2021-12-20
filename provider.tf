912c757bd78b611eeea2456881cf61953445b7c4
Nuno Gonçalves <nuno.goncalves@oracle.com>
2021-12-20 18:41:10 +0100

    remove hardcoded provider version

--- provider.tf
+++ provider.tf
@@ -1,16 +1,6 @@
 ## Copyright (c) 2021, Oracle and/or its affiliates.
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
-terraform {
-  required_version = ">= 0.15.0"
-  required_providers {
-    oci = {
-      source  = "hashicorp/oci"
-      version = "4.48.0"
-    }
-  }
-}
-
 provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
